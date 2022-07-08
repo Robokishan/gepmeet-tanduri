9a8be64b597c23b57418e3414ab62a0fa1e0e635
Kishan Joshi <robokishan.blogspot@gmail.com>
2022-07-08 21:07:39 +0530

    fix: 🐛 rabbitmq crash because noAck

--- src/client/rabbitmq/index.ts
+++ src/client/rabbitmq/index.ts
@@ -11,24 +11,33 @@ const retryInterval = 5000;
 let rabbitMQConnection: Connection = null;
 let rabbitMQChannel: Channel = null;
 const startRabbit = async () => {
-  let conn: Connection = null;
-  try {
-    conn = await amqp.connect(config.RABITMQ_URL);
-    conn.on('close', async function (err: Error) {
-      log.error('Rabbit connection closed with error: ', err);
-      setTimeout(async () => await startRabbit(), retryInterval);
-    });
-    rabbitMQConnection = conn;
-    log.info(`Rabbitmq Connection Successfull`);
-    rabbitMQChannel = await conn.createChannel();
-    await createQueues(rabbitMQChannel);
-    await createExchanges(rabbitMQChannel);
+  return new Promise((resolve, _reject) => {
+    log.info('Rabbitmq Connecting');
+    amqp
+      .connect(config.RABITMQ_URL)
+      .then(async (conn: Connection) => {
+        conn.on('close', async (err: Error) => {
+          log.error('Rabbit connection closed', err);
+        });
+        conn.on('error', async (err: Error) => {
+          log.error('Rabbit connection error: ', err);
+          conn.close();
+          conn.removeAllListeners();
+          setTimeout(async () => await startRabbit(), retryInterval);
+        });
+        rabbitMQConnection = conn;
+        log.info(`Rabbitmq Connection Successfull`);
+        rabbitMQChannel = await conn.createChannel();
+        await createQueues(rabbitMQChannel);
+        await createExchanges(rabbitMQChannel);
+        resolve(conn);
+      })
+      .catch((err) => {
+        log.error(err);
+        setTimeout(async () => await startRabbit(), retryInterval);
+      });
     // await startRPCServer(conn, 'gepmeet-tanduri');
-  } catch (error) {
-    log.error(error);
-  }
-
-  return conn;
+  });
 };
 
 export { rabbitMQConnection, startRabbit, rabbitMQChannel };
--- src/client/rabbitmq/queues.ts
+++ src/client/rabbitmq/queues.ts
@@ -2,5 +2,7 @@ import { Channel } from 'amqplib';
 import { QUEUE_ROOM_EVENTS } from '../../utils/constant';
 
 export const createQueues = async (channel: Channel) => {
-  await channel.assertQueue(QUEUE_ROOM_EVENTS);
+  await channel.assertQueue(QUEUE_ROOM_EVENTS, {
+    // durable: false
+  });
 };
